9b6984f6f242c96fc290c424200d8c1f0f4d5fbe
Leah Mattern <ladyleahmattern@gmail.com>
2021-03-13 19:22:24 -0500

    Added media card to the media page as connection test.

--- src/pages/MediaPage/index.js
+++ src/pages/MediaPage/index.js
@@ -1,8 +1,10 @@
+import MediaCard from '../../components/MediaCard/index';
 
 function MediaPage() {
     return (
         <div>
             <h1>Media Page</h1>
+            <MediaCard />
         </div>
     )
 }

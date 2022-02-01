164bc45b3786eced0b2d816e6197d1ac7be8618f
Leah Mattern <ladyleahmattern@gmail.com>
2022-01-31 22:19:52 -0500

    Setting up global tyles and contact info content for profile page

--- src/pages/ProfilePage/index.js
+++ src/pages/ProfilePage/index.js
@@ -18,12 +18,18 @@ function ProfilePage() {
                 <h1>Profile Page</h1>
                 <Image id="profile-image" image={leah} alt="Leah Mattern's profile picture" />
                 <div id="contact-info">
-                    <p>Linkedin Logo Here Soon</p>
-                    <a href='https://www.linkedin.com/in/lady-leah-mattern/'>Linkedin</a>
-                    <p>GitHub Logo Coming Soon</p>
-                    <a href='https://github.com/Lady-Mattern'>GitHub</a>
-                    <p>Email Logo Coming Soon</p>
-                    <a href="mailto: ladyleahmattern@gmail.com">Email</a>
+                    <div className='flex-row'>
+                        <p>Linkedin Logo Here Soon</p>
+                        <a href='https://www.linkedin.com/in/lady-leah-mattern/'>Linkedin</a>
+                    </div>
+                    <div className='flex-row'>
+                        <p>GitHub Logo Coming Soon</p>
+                        <a href='https://github.com/Lady-Mattern'>GitHub</a>
+                    </div>
+                    <div className='flex-row'>
+                        <p>Email Logo Coming Soon</p>
+                        <a href="mailto: ladyleahmattern@gmail.com">Email</a>
+                    </div>
                 </div>
                 <Resume />
             </main>
